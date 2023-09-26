efd8384aa3c911e1718db6a5048e760e4a6232f9
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2023-09-26 18:55:10 +0200

    corr retry4

--- ex03/ft_strjoin.c
+++ ex03/ft_strjoin.c
@@ -6,21 +6,37 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 11:29:21 by ade-beco          #+#    #+#             */
-/*   Updated: 2023/09/21 11:29:22 by ade-beco         ###   ########.fr       */
+/*   Updated: 2023/09/26 18:54:29 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-// #include <stdio.h>
+/*#include <stdio.h>*/
 
-int	ft_strlen(char *str)
+int	ft_malloclen(char **strs, char *sep, int size)
 {
 	int	i;
+	int	k;
+	int lenght;
 
 	i = 0;
-	while (str[i] != '\0')
+	k = 0;
+	lenght = 0;
+	while (sep[lenght] != '\0')
+		lenght++;
+	lenght *= (size - 1);
+	i = 0;
+	while (i < size)
+	{
+		k = 0;
+		while (strs[i][k] != '\0')
+		{
+			k++;
+			lenght++;
+		}
 		i++;
-	return (i);
+	}
+	return (lenght + 1);
 }
 
 char	*ft_strjoin(int size, char **strs, char *sep)
@@ -33,11 +49,11 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	i = 0;
 	k = 0;
 	l = 0;
-	if (size == 0)
-		return ("/0");
-	out = malloc(sizeof(strs) + (sizeof(sep) * (size - 1)) + 1);
+	out = malloc(ft_malloclen(strs, sep, size));
 	if (!out)
 		return (NULL);
+	if (size == 0)
+		return (out);
 	while (i < size)
 	{
 		k = 0;
